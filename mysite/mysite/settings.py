549650fbaf182e324a544be7671aaa77e082b8b6
jtmacoco <jtmacoco@mail.csuchico.edu>
2021-12-07 04:56:35 +0000

    GCP

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4b__g)2+dwmd!ml&fnr37j(h$!f)@!z6&e@pgk_$a4eu=)*l$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["35.208.99.114"]
 
 
 # Application definition
